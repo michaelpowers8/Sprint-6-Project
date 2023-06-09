9a5f38659982cbe5989f22cd033a9f17036dd47f
Michael Powers <126989418+michaelpowers8@users.noreply.github.com>
2023-06-08 21:41:31 -0500

    Create sql_task_03.sql

---
+++ sql_task_03.sql
@@ -0,0 +1,19 @@
+SELECT
+    CASE 
+        WHEN company_name = 'Flash Cab' THEN 'Flash Cab' 
+        WHEN company_name = 'Taxi Affiliation Services' THEN 'Taxi Affiliation Services' 
+        ELSE 'Other' 
+    END AS company,
+    COUNT(trips.trip_id) as trips_amount                
+FROM 
+    cabs
+INNER JOIN 
+    trips 
+ON 
+    trips.cab_id = cabs.cab_id
+WHERE 
+    CAST(trips.start_ts AS date) BETWEEN '2017-11-01' AND '2017-11-07'
+GROUP BY 
+    company
+ORDER BY 
+    trips_amount DESC;
